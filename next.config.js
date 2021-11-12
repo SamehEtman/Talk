062eca9a254188f72a24d31c9ea4b87aaec1bd1e
Sameh <samehothman20162017@gmail.com>
2021-11-12 02:38:56 +0200

    add config file

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DB_URL: ``,
+  },
+  images: {
+    domains: [
+      'www.w3schools.com',
+      'upload.wikimedia.org',
+      'static.wikia.nocookie.net',
+      'en.wikipedia.org',
+      'via.placeholder.com'
+    ],
+  },
+};
