807be170fe5313b67a012b188313273c41f0b0e5
Sameh <samehothman20162017@gmail.com>
2021-11-14 00:21:24 +0200

    fix a small state bug

--- components/home/Welcome.js
+++ components/home/Welcome.js
@@ -2,28 +2,24 @@ import classes from './Welcome.module.css';
 import { getSession, useSession } from 'next-auth/client';
 import Link from 'next/dist/client/link';
 import { useEffect, useState } from 'react';
-function Welcome({sign , msg , browse }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+function Welcome({ sign, msg, browse }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   useEffect(() => {
     getSession().then((session) => {
-      if (session) setIsLoggedIn(true);
-      else setIsLoggedIn(false);
+      if (session) setIsLoggedIn('yes');
+      else setIsLoggedIn('no');
     });
   }, []);
 
   return (
     <section className={classes.starting}>
       <h1>{msg}</h1>
-      {!isLoggedIn && (
-        <h1 className={classes.link}>
-          <Link href="/auth">{sign}</Link>
-        </h1>
-      )}
-      {isLoggedIn && (
-        <h1 className={classes.link}>
-          <Link href="/posts">{browse}</Link>
-        </h1>
-      )}
+
+      <h1 className={classes.link}>
+        <Link href="/posts">
+          {isLoggedIn === 'yes' ? browse : isLoggedIn === 'no' ? sign : ''}
+        </Link>
+      </h1>
     </section>
   );
 }
